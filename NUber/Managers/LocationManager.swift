e1c51193a2d9417d61bfd980df65a77cd15068c6
David Newman <david.newman.wv@gmail.com>
2022-04-21 21:08:07 -0400

    Initial Commit

---
+++ NUber/Managers/LocationManager.swift
@@ -0,0 +1,70 @@
+//
+//  LocationManager.swift
+//  NUber
+//
+//  Created by David Newman on 4/20/22.
+//
+
+import Foundation
+import CoreLocation
+
+struct LocationAddress {
+    var location: CLLocation
+    var city: String?
+    var country: String?
+    var address: String?
+    
+    init(location: CLLocation){
+        self.location = location
+    }
+}
+
+
+
+typealias handler = () -> ()?
+
+class LocationManager: NSObject {
+   
+    public static let shared = LocationManager()
+    
+    var locationUpdated: handler?
+    
+    var currentLocation: LocationAddress?
+    
+    private var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 10
+        
+        return locationManager
+        
+    }()
+    
+    func startLocationUpdate(completed: handler?){
+        locationUpdated = completed
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        if let lastLocation = locations.last {
+//            print("Lat : \(lastLocation.coordinate.latitude) \nLng : \(lastLocation.coordinate.longitude)")
+//        }
+        guard let firstLocation = locations.first else {return}
+        
+        if CLLocationCoordinate2DIsValid(firstLocation.coordinate){
+            currentLocation = .init(location: firstLocation)
+            if let update = locationUpdated{
+                update()
+            }
+            
+        }
+    }
+    
+}
+
+
